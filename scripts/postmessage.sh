55ffe51e4c563bd693602457761e2961049e9698
chungthuang <7826979+chungthuang@users.noreply.github.com>
2020-03-28 16:09:52 +0800

    Remove scripts

--- scripts/postmessage.sh
+++
@@ -1,4 +0,0 @@
-curl -v https://slack.com/api/chat.postMessage \
-    -H 'Content-Type: application/json' \
-    -H 'Authorization: Bearer xoxb-732916575875-976244796035-JvvdbaysxdGnSGzAQ4cOyAne' \
-    -d @postmessage.json
